3f83b8713cd1117933f17ecc77014de9c021724f
Rammah Karpous <rammahkarpous@outlook.com>
2025-04-11 15:01:09 +0200

    fix: all users create products for user with id 1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,49 +8,56 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
 
-    public function validate($data) {
-        return $data->validate([
-            'user_id' => ['required'],
-            'product_name' => ['string', 'required'],
-            'kcal' => ['numeric', 'min:0', 'required'],
-            'fat' => ['numeric', 'min:0', 'required'],
+    public function validate( $data )
+    {
+        return $data->validate( [
+            'user_id'       => ['required'],
+            'product_name'  => ['string', 'required'],
+            'kcal'          => ['numeric', 'min:0', 'required'],
+            'fat'           => ['numeric', 'min:0', 'required'],
             'saturated_fat' => ['numeric', 'min:0', 'required'],
-            'carbs' => ['numeric', 'min:0', 'required'],
-            'protein' => ['numeric', 'min:0', 'required'],
-        ]);
+            'carbs'         => ['numeric', 'min:0', 'required'],
+            'protein'       => ['numeric', 'min:0', 'required'],
+        ] );
     }
 
-    public function index() {
+    public function index()
+    {
         $user = auth()->user();
 
-        return inertia('products/add-product', [
-            'products' => Product::where('user_id', $user->id)->get(),
-        ]);
+        return inertia( 'products/add-product', [
+            'products' => Product::where( 'user_id', $user->id )->get(),
+        ] );
     }
 
-    public function create(Request $request) {        
-        $validation = $this->validate($request);
-        Product::create($validation);
+    public function create( Request $request )
+    {
+        $request->merge( [ 'user_id' => auth()->user()->id ] );
+        $validation = $this->validate( $request );
+        Product::create( $validation );
 
-        return redirect()->route('product.index')->with('success','Product added!');
+        return redirect()->route( 'product.index' )->with( 'success', 'Product added!' );
     }
 
-    public function delete($id) {
-        Product::destroy($id);
+    public function delete( $id )
+    {
+        Product::destroy( $id );
     }
 
-    public function update(Request $request, $id) {
-        $validation = $this->validate($request);
-        Product::find($id)->update($validation);
+    public function update( Request $request, $id )
+    {
+        $validation = $this->validate( $request );
+        Product::find( $id )->update( $validation );
 
-        return redirect()->route('product.index')->with('success','Product updated!');
+        return redirect()->route( 'product.index' )->with( 'success', 'Product updated!' );
     }
 
-    public function calculator() {
+    public function calculator()
+    {
         $user = auth()->user();
 
-        return inertia('products/calculator', [
-            'products' => Product::where('user_id', $user->id)->get(),
-        ]);
+        return inertia( 'products/calculator', [
+            'products' => Product::where( 'user_id', $user->id )->get(),
+        ] );
     }
 }
