0399a1a1777010c0ce4f0479737fe63e41b308fa
Rammah Karpous <rammahkarpous@outlook.com>
2025-06-01 22:15:50 +0200

    feat: add a max height to the products modal

--- resources/js/components/organisms/ProductsModal.tsx
+++ resources/js/components/organisms/ProductsModal.tsx
@@ -29,37 +29,39 @@ export default function ProductsModal({ products, className, setShowModal }: Pro
   });
 
   return (
-    <div className={`bg-background-alt absolute top-1/2 left-1/2 max-h-fit w-full max-w-xl -translate-x-1/2 -translate-y-1/2 overflow-y-scroll rounded-xl p-5 shadow-2xl ${className}`}>
+    <div className={`bg-background-alt absolute top-1/2 left-1/2 max-h-fit w-full max-w-xl -translate-x-1/2 -translate-y-1/2 overflow-y-hidden rounded-xl p-5 shadow-2xl ${className}`}>
       <div className="flex items-center justify-between">
         <h4 className="text-2xl">Select products</h4>
         <X className="cursor-pointer" onClick={() => setShowModal(false)} />
       </div>
 
       <div className="mt-5">
-        {products.map((product) => (
-          <div key={product.id} className="border-background-alt flex items-center gap-2 border-b py-1">
-            <input 
-              type="checkbox" 
-              name="products" 
-              id={product.product_name} 
-              value={product.product_name} 
-              className="mr-3 absolute opacity-0 peer"
-              checked={selectedProductsList.some(item => item.id === product.id)}
-              onChange={() => {
-                if (selectedProductsList.some(item => item.id === product.id)) {
-                  setSelectedProductsList(selectedProductsList.filter((item) => item.id !== product.id))
-                } else {
-                  setSelectedProductsList([...selectedProductsList, product])
-                }
-              }} />
-            <label 
-              htmlFor={product.product_name} 
-              className='peer-checked:bg-background/25 bg-background/10 peer-checked:text-white p-5 w-full rounded-lg cursor-pointer border border-background/50'>
-              
-              {product.product_name}
-            </label>
-          </div>
-        ))}
+        <div className='max-h-[500px] overflow-y-auto'>
+          {products.map((product) => (
+            <div key={product.id} className="border-background-alt flex items-center gap-2 border-b py-1">
+              <input 
+                type="checkbox" 
+                name="products" 
+                id={product.product_name} 
+                value={product.product_name} 
+                className="mr-3 absolute opacity-0 peer"
+                checked={selectedProductsList.some(item => item.id === product.id)}
+                onChange={() => {
+                  if (selectedProductsList.some(item => item.id === product.id)) {
+                    setSelectedProductsList(selectedProductsList.filter((item) => item.id !== product.id))
+                  } else {
+                    setSelectedProductsList([...selectedProductsList, product])
+                  }
+                }} />
+              <label 
+                htmlFor={product.product_name} 
+                className='peer-checked:bg-background/25 bg-background/10 peer-checked:text-white p-5 w-full rounded-lg cursor-pointer border border-background/50'>
+                
+                {product.product_name}
+              </label>
+            </div>
+          ))}
+        </div>
 
         <button 
           className="mt-5 rounded-lg bg-orange-600 py-3 px-6 cursor-pointer" 
